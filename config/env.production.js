964e01540fa22d7c1c3d94d9faed1a685c794c0a
Surinder Kumar <Surinder@ferventsoft.com>
2020-10-14 16:12:14 +0530

    change email user password

--- config/env.production.js
+++ config/env.production.js
@@ -8,10 +8,11 @@ export const SMTP_HOST ='smtp.office365.com';
 export const SMTP_PORT =587;
 export const SMTP_USERNAME = 'assist@everyurban.com';
 // export const SMTP_PASSWORD = 'Assisteveryurban@123';
-export const SMTP_PASSWORD = 'Temporary@123';
+export const SMTP_PASSWORD = 'Temporary@everyurban123';
+// 'Temporary@123';
 export const PRO_ENQUIRY_BccEmail = 'assist@everyurban.com';
 export const CONATACTUS_TO_EMAIL = 'assist@everyurban.com';//for contactus
 export const SMTP_SECURE = false;
 export const postgres = {username:'postgres',server:'localhost',password:'everyurban2020',database:'every_urban'};
 export const hostName ="https://everyurban.com";
-export const privateLocalAddress ="https://localhost:8081";
+export const privateLocalAddress ="http://localhost:8081";
